1db05bcea1d2d3ba818ee665e9700312f495c4e2
SatyamA007 <satyamawasthi007@gmail.com>
2023-01-26 21:45:03 -0800

    Body stabilised and head positioned testing

--- Assets/Scripts/RestoreOriginRotation.cs
+++ Assets/Scripts/RestoreOriginRotation.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class RestoreOriginRotation : MonoBehaviour
 {
+    public GameObject bodyObj;
     private Quaternion lastParentRotation;
+    private Quaternion lastBodyRot;
+    private bool set = true;
+
     // Start is called before the first frame update
     void Start()
     {
         lastParentRotation = transform.parent.localRotation;
+        lastBodyRot = bodyObj.transform.localRotation;
     }
 
     // Update is called once per frame
@@ -16,8 +21,18 @@ public class RestoreOriginRotation : MonoBehaviour
     {
         transform.localRotation = Quaternion.Inverse(transform.parent.localRotation)*
             lastParentRotation*
+            bodyObj.transform.localRotation*
+            Quaternion.Inverse(lastBodyRot)*
             transform.localRotation;
 
+            
+        lastBodyRot = bodyObj.transform.localRotation;
         lastParentRotation = transform.parent.localRotation;
+        if(set){
+            //before first frame bodyRot is always 0,0,0
+            transform.localRotation *= Quaternion.Inverse(lastBodyRot);
+            set = false;
+        }
+            
     }
 }
